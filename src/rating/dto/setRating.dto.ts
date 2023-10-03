88345207a5b72e550db0beb9bb816a1f43739609
Egor Dubovik <egorka54896@gmail.com>
2023-10-03 18:26:57 +0300

    feet: add methods getMovieValueByUser and setRating

---
+++ src/rating/dto/setRating.dto.ts
@@ -0,0 +1,9 @@
+import { IsNumber, IsString } from 'class-validator';
+import { Types } from 'mongoose';
+
+export class SetRatingDto {
+	@IsString()
+	movieId: Types.ObjectId;
+	@IsNumber()
+	value: number;
+}
